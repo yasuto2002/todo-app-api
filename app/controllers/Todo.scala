b9a896fd2ea01aa0460a1f4aadc4d998ac845404
yasuto <fujiya0101@gmail.com>
2023-06-08 17:56:12 +0900

    update:TODOリストUI作成

--- app/controllers/Todo.scala
+++ app/controllers/Todo.scala
@@ -2,11 +2,11 @@ package controllers
 import play.api.mvc.{BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
-import model.ViewValueHome
+import model.ViewValueTodoList
 @Singleton
 class TodoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   def index() = Action { implicit req =>
-    val vv = ViewValueHome(
+    val vv = ViewValueTodoList(
       title = "TODOリスト",
       cssSrc = Seq("main.css","todoList.css"),
       jsSrc = Seq("main.js")
