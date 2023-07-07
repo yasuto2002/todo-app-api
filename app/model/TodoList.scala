1e318fae0b13fb20ea0b11cfcf89775cb95bfe98
yasuto <fujiya0101@gmail.com>
2023-07-07 14:22:36 +0900

    fix:TodoForm 親パッケージをimportするように修正

--- app/model/TodoList.scala
+++ app/model/TodoList.scala
@@ -1,9 +1,11 @@
 package model
 
+import lib.model.{Category, Todo}
+
 case class ViewValueTodoList(
   title:  String,
   cssSrc: Seq[String],
   jsSrc:  Seq[String],
 ) extends ViewValueCommon
 
-case class TodoForm(category:lib.model.Category.Id,title:String,body:String,State:lib.model.Todo.Status)
+case class TodoForm(category:Category.Id,title:String,body:String,State:Todo.Status)
