da49156ecfb199968bf795a49ce20f27521d5eb6
yasuto <fujiya0101@gmail.com>
2023-08-09 15:37:14 +0900

    fix:列挙型の命名

--- app/json/writes/JsValueCategoryListItem.scala
+++ app/json/writes/JsValueCategoryListItem.scala
@@ -1,22 +1,35 @@
 package json.writes
 
-import play.api.libs.json.{Json, Writes}
+import ixias.util.json.JsonEnvWrites
+import lib.model
+import lib.model.Category.Color
+import lib.model.{Category, Todo}
+import play.api.libs.json.{JsNumber, JsValue, Json, Writes}
 
 case class JsValueCategoryListItem(
-  id:       Long,
-  name:    String,
-  slug:     String,
-  color_id:    Short,
+  id:         Category.Id,
+  name:       String,
+  slug:       String,
+  color_code: Category.Color,
 )
 
-object JsValueCategoryListItem{
+object JsValueCategoryListItem extends JsonEnvWrites {
+
+  implicit val categoryIdWrites: Writes[Category.Id] = new Writes[Category.Id] {
+    def writes(tag: Category.Id) = JsNumber(tag)
+  }
+
+  implicit val colorWrites: Writes[Category.Color] = new Writes[Category.Color] {
+    def writes(color: Category.Color): JsValue = EnumStatusWrites.writes(color)
+  }
+
   implicit val writes: Writes[JsValueCategoryListItem] = Json.writes[JsValueCategoryListItem]
 
   def apply(category: lib.model.Category.EmbeddedId): JsValueCategoryListItem =
     JsValueCategoryListItem(
-      id = category.id,
-      name = category.v.name,
-      slug = category.v.slug,
-      color_id = category.v.color.code,
+      id          = category.id,
+      name        = category.v.name,
+      slug        = category.v.slug,
+      color_code  = category.v.color,
     )
   }
