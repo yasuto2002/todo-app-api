c5a2bf06d84c3e7109a8b4b34f08799c13cc91b4
yasuto <fujiya0101@gmail.com>
2023-08-23 16:05:54 +0900

    fix:todo一覧レスポンス値修正

--- app/json/writes/JsValueTodoListItem.scala
+++ app/json/writes/JsValueTodoListItem.scala
@@ -8,7 +8,7 @@ case class JsValueTodoListItem(
   title:          String,
   body:           String,
   state_code:     Todo.Status,
-  category_name:  String
+  category:       Category.EmbeddedId
 )
 
 object JsValueTodoListItem extends JsonEnvWrites {
@@ -21,6 +21,10 @@ object JsValueTodoListItem extends JsonEnvWrites {
     def writes(st: Todo.Status): JsValue = EnumStatusWrites.writes(st)
   }
 
+  implicit val categoryWrites: Writes[Category.EmbeddedId] = new Writes[Category.EmbeddedId] {
+    def writes(category: Category.EmbeddedId): JsValue = Json.toJson(JsValueCategoryListItem(category))
+  }
+
   implicit val writes: Writes[JsValueTodoListItem] = Json.writes[JsValueTodoListItem]
   def apply(todo: Todo.EmbeddedId, category: Category.EmbeddedId): JsValueTodoListItem =
     JsValueTodoListItem(
@@ -28,6 +32,6 @@ object JsValueTodoListItem extends JsonEnvWrites {
       title           = todo.v.title,
       body            = todo.v.body,
       state_code      = todo.v.state,
-      category_name   = category.v.name
+      category        = category
     )
 }
